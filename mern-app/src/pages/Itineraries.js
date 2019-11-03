a31005370c5d8373ad3ea4ee0e7a3987905bb76d
Jhurgen Maraza <JhurgenMrz99@gmail.com>
2019-11-03 10:26:44 -0300

    Fetch itineraries by City in CitiesPage

--- mern-app/src/pages/Itineraries.js
+++ mern-app/src/pages/Itineraries.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
+import axios from 'axios'
 import {Link} from 'react-router-dom'
 import '../styles/Itineraries.css'
 
 export const Itineraries = (props) => {
     console.log(props.match.params.city_name)
-    console.log(props.match.params._id)
+    const cityId = props.match.params._id
+
+    const [itineraries, setItineraries] = useState([])
+
+    useEffect(()=>{
+        axios(`http://localhost:5001/api/itineraries/${cityId}`).then(({data})=>{
+            console.log(data.data)
+            setItineraries(data.data)
+            
+        })
+        .catch(err=> console.log(err))
+    },[])
+
+    function showStarts(count){
+        const starts= []
+        for(let i = 0; i < count; i++){
+            starts.push('star')
+        }
+        return starts
+    }
+
+
     return (
         <div className="Itineraries__container">
             <section className="Itineraries__nav"> 
@@ -13,12 +35,27 @@ export const Itineraries = (props) => {
                 </Link>
                 <h3>Itineraries</h3>
             </section>
-            <table>
+            <table style={{textAlign: "center"}}>
                 <thead>
                     <th>Activity</th>
                     <th>duration</th>
                     <th>rating</th>
                 </thead>
+                <tbody>
+                    {
+                        itineraries.map(item=>(
+                            <tr>
+                                <td>{item.title}</td>
+                                <td>{item.duration}m</td>
+                                <td>
+                                    {
+                                        showStarts(item.rating).map(el=> <div>⭐</div>)
+                                    }
+                                </td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
             </table>
         </div>
     )
