481810adca32a899051035f8094a8b40cc58bcbb
Jhurgen Maraza <JhurgenMrz99@gmail.com>
2019-12-05 10:29:27 -0300

    Update for deploy

--- backend/config/index.js
+++ backend/config/index.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const config = {
   dev: !process.env.NODE_ENV === 'production',
   SecretKey: process.env.SECRET,
-  apiKeyTokenPublic: process.env.API_KEY_TOKEN_PUBLIC,
   googleIdClient: process.env.GOOGLE_ID_CLIENT,
   googleSecretClient: process.env.GOOGLE_SECRET_CLIENT
 };
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
+const helmet = require('helmet')
 const cors = require('cors');
-
 require('dotenv').config();
 require('./lib/mongodb');
 const citiesApi = require('./routes/api/cities');
@@ -12,19 +11,30 @@ const usersApi = require('./routes/api/users');
 const authApi = require('./routes/api/auth');
 const userItineraries = require('./routes/api/userItineraries');
 const passport = require('passport');
+const { join } = require('path')
+const app = express();
 
 //Middlewares
 app.use(cors());
+app.use(helmet())
 app.use(passport.initialize());
 app.use(bodyParser.json());
 
+//Static Files
+app.use(express.static(join(__dirname, 'client','build')))
+
 //Routes
+// Routes API
 citiesApi(app);
 itinerariesApi(app);
 activitiesApi(app);
 usersApi(app);
 authApi(app);
 userItineraries(app)
+// Render Index.html
+app.get('/', (req,res)=>{
+    res.status(200).sendFile(join(__dirname,'client','build','index.html'))
+})
 
 const port = process.env.PORT || 5001;
 app.listen(port, () => console.log(`Server on Port http://localhost:${port}`));
