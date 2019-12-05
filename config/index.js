2d5d972cbdb7d59a38b95142fd5c6672764a0a2a
Jhurgen Maraza <JhurgenMrz99@gmail.com>
2019-12-05 11:41:06 -0300

    Update settings

--- config/index.js
+++ config/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const config = {
   dev: !process.env.NODE_ENV === 'production',
+  mongodbPassword: process.env.PASSWORD_DB,
   SecretKey: process.env.SECRET,
   googleIdClient: process.env.GOOGLE_ID_CLIENT,
   googleSecretClient: process.env.GOOGLE_SECRET_CLIENT
